35bcffb9e4834f1a35cc5ff2ac14511ca6d02903
technoprogramming92 <technoprogramming92@gmail.com>
2023-05-08 23:01:05 +0530

    Testimonial completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero"
 import OurServices from "@/components/OurServices"
+import Testimonials from "@/components/Testimonials"
 import Whys from "@/components/Whys"
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <OurServices />
       <Whys />
+      <Testimonials />
     </div>
   )
 }
