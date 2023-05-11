1c2d02ff474b3fcb7e80ed6db637de55b47a7ca4
Purvi Trivedi <ignitax@gmail.com>
2023-05-11 22:57:29 +0530

    error change by p

--- components/Services/serviceData.tsx
+++ components/Services/serviceData.tsx
@@ -6,179 +6,108 @@ const serviceData: Service[] = [
     title: "Bookkeeping",
     paragraph:
       "we offer comprehensive bookkeeping services, delivered virtually by a dedicated person or team of professionals to help you stay organised and in control of your finances.",
-<<<<<<< HEAD
     image: "/images/all services/Book-Keeping.svg",
-=======
-    image: "/images/services-images/Tax-bro.svg",
     path: "/services/tax",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
+
   },
   {
     id: 2,
     title: "Tax Planning & Preparation",
     paragraph:
       "Say goodbye to stress of tax prepration and tax planning with Ignitax! Our reliable tech and virtual staff of superheroes (EA's & CPA's) will make sure you never miss a payment or deadline again.",
-<<<<<<< HEAD
+
     image: "/images/all services/tax home.svg",
-=======
-    image: "/images/services-images/Tax-amico.svg",
     path: "/services/tax",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 3,
     title: "Software Development",
     paragraph:
       "Our virtual professionals are pros in web development, software development, and automation. From stunning website designs to magical software integrations, we are here to help you.",
-<<<<<<< HEAD
+
     image: "/images/all services/softandweb.svg",
-=======
-    image: "/images/services-images/Design team-amico.svg",
     path: "/services/webdev",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 4,
     title: "Website Development",
     paragraph:
-<<<<<<< HEAD
       "We craft custom websites that are tailored to your business, utilizing top-notch technology to deliver a responsive, user-friendly, and search engine optimized experience.",
     image: "/images/all services/website development.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/Website Creator-amico.svg",
     path: "/services/webdev",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 5,
     title: "IT & Support",
     paragraph:
-<<<<<<< HEAD
+
       "We offer excellent IT and support services for businesses like yours. Whether you need help with a virus, software updates, or expert advice, we've got you covered.",
     image: "/images/all services/it support.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/Code typing-amico.svg",
     path: "/services/itsupport",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
+
   },
   {
     id: 6,
     title: "Digital Media Marketing",
     paragraph:
-<<<<<<< HEAD
       "Struggling to get any response or engagement on social media? Our digital media experts will boost your online presence and connect with your audience through content creation and social media management.",
     image: "/images/all services/degital media marketing.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/Marketing consulting-pana.svg",
-    path: "/services/seo",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 7,
     title: "Content Writing",
     paragraph:
-<<<<<<< HEAD
       "Need help crafting content that captures your audience's attention and drives them to action? Our team of expert wordsmiths is here to help! We'll create compelling copy that tells your brand story, informs and entertains your audience, and motivates them to take action.",
     image: "/images/all services/content writing.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/Creative writing-amico.svg",
-    path: "/services/contentwriting",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 8,
     title: "Graphic Designing",
     paragraph:
-<<<<<<< HEAD
       "We bring your brand to life with stunning graphics and designs that capture its essence. Our talented team of visual artists can create a logo, brochure, or website design that represents your brand and makes an impact.",
     image: "/images/all services/Graphic design.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/Designer-pana.svg",
-    path: "/services/graphicdesign",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 9,
     title: "Programmers",
     paragraph:
-<<<<<<< HEAD
       "Looking for a dependable development partner? Look no further. Get in touch with us now to discover more about our development services and how we can bring your ideas to life.",
     image: "/images/all services/programmer and developers.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/Programmer-bro.svg",
-    path: "/services/developers",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 10,
     title: "Photography",
     paragraph:
-<<<<<<< HEAD
       "We offer top-quality photography services to capture your special moments in exceptional detail, whether it's for products or events.",
     image: "/images/all services/photography.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/Studio photographer-amico.svg",
-    path: "/services/photography",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 11,
     title: "Admin & Assistant",
     paragraph:
-<<<<<<< HEAD
       "Our virtual assistants excel in managing schedules, emails, bookkeeping, data entry, and more. We prioritize your unique needs with meticulous attention to detail.",
     image: "/images/all services/Admin work and assistance.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/Edit photo-bro.svg",
-    path: "/services/editing",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 12,
     title: "Search Engine Optimization",
     paragraph:
-<<<<<<< HEAD
       "Our SEO virtual services at IGNITAX enhance your website's visibility and attractiveness to search engines by optimizing its content and structure.",
     image: "/images/all services/SEO.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/Admin-bro.svg",
-    path: "/services/adminwork",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 13,
     title: "Editing",
     paragraph:
-<<<<<<< HEAD
       "Want to turn your raw footage and images into breathtaking works of art? Let our team of editing experts take your visual content to the next level. From color grading to special effects, we'll bring your vision to life and make your photos and videos stand out from the crowd. Contact us today to learn more about our editing services and how we can help you create stunning visual content.",
     image: "/images/all services/Edit photo.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/SEO analytics team-rafiki.svg",
-    path: "/services/seo",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
   {
     id: 14,
     title: "Mobile Application Development",
     paragraph:
-<<<<<<< HEAD
       "We excel at developing customized, trendy, and high-performing mobile apps that are user-friendly, reliable, and seamless.",
     image: "/images/all services/Mobile app development.svg",
-=======
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/services-images/App development-bro.svg",
-    path: "/services/webdev",
->>>>>>> 1bd4209db3facadf7f4b6635cd323babad393750
   },
 ];
 export default serviceData;
