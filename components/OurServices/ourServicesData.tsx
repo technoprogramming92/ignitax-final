e50f71f6f7f0a7f577780e2be85442924f77d4a3
technoprogramming92 <technoprogramming92@gmail.com>
2023-05-17 22:39:57 +0530

    Full and Final commit2

--- components/OurServices/ourServicesData.tsx
+++ components/OurServices/ourServicesData.tsx
@@ -7,7 +7,7 @@ const ourServicesData: Service[] = [
     paragraph:
       "Say goodbye to the stress of bookkeeping, tax preparation and tax planning with Ignitax! Our reliable tech and virtual staff of superheroes (EA's & CPA's) will make sure you never miss a payment or deadline again. Schedule a free consultation today and let us make bookkeeping and taxes fun! (Okay, maybe not fun, but definitely less painful!)",
     image: "/images/all services/tax home.svg",
-    path: "/services/tax",
+    path: "/tax",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const ourServicesData: Service[] = [
     paragraph:
       "Are you stuck with software and websites from the dinosaur era? Let us transport you to the future! Our virtual team of professionals specialise in web development, software development, and automation. From stunning website designs to magical software integrations, we've got you covered. Say goodbye to clunky, outdated tech and hello to a brighter future with Ignitax.",
     image: "/images/all services/website development.svg",
-    path: "/services/webdev",
+    path: "/webdev",
   },
   {
     id: 3,
@@ -23,7 +23,7 @@ const ourServicesData: Service[] = [
     paragraph:
       "Are you tired of dealing with slow websites, finicky databases, and glitchy hardware? we've got your back with our best virtual staff of IT and support services. From website and database maintenance to IoT and cloud management our virtual assistant team and networking and hardware support team is with you. Why stress when you can contact us today for a free consultation?",
     image: "/images/all services/it support.svg",
-    path: "/services/itsupport",
+    path: "/itsupport",
   },
   {
     id: 4,
@@ -31,7 +31,7 @@ const ourServicesData: Service[] = [
     paragraph:
       "Are you feeling lost in the digital jungle? Take a deep breath Ignitax is here to guide you through the wilderness with our expert digital media marketing services! Our team of virtual marketing gurus specialize in social media marketing, SEO, email marketing, content creation, and much more. From increasing your brand awareness to boosting your online presence, we've got you covered. Contact us today and let us help you stand out from the digital herd!",
     image: "/images/all services/degital media marketing.svg",
-    path: "/services/seo",
+    path: "/seo",
   },
   {
     id: 5,
@@ -39,7 +39,7 @@ const ourServicesData: Service[] = [
     paragraph:
       "Hmm! tired of writing bland content that puts even you to sleep? Don't worry, for Ignitax's team of witty wordsmiths is here to save the day! We'll create content that's so engaging, your audience will be hanging on your every word. From blog posts to product descriptions, our team can do it all. And the best part? You'll never have to face writer's block again! So sit back, relax, and let us do the writing.To create some magic together, Drop us a line and let's start crafting some killer content.",
     image: "/images/all services/Creative writing home.svg",
-    path: "/services/contentwriting",
+    path: "/contentwriting",
   },
   {
     id: 6,
@@ -47,7 +47,7 @@ const ourServicesData: Service[] = [
     paragraph:
       "Say goodbye to mediocre design and hello to eye-catching graphics with Ignitax's top-notch graphic design services! Our talented virtual designers specialize in everything from logo design to web graphics, and we're here to bring your visions to life. Need a design refresh or a new logo? We've got you covered. With Ignitax, your brand will be unstoppable. Schedule a free consultation today and let's make your visuals pop!",
     image: "/images/all services/Graphic design.svg",
-    path: "/services/graphicdesign",
+    path: "/graphicdesign",
   },
 ];
 
